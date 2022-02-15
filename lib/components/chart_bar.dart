9ce9ad9faeee5349f634c1af8104d2083dce2ad7
adrianoneto <53943577+adrianoneto@users.noreply.github.com>
2022-02-15 11:39:00 -0300

    melhorando grafico

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -15,7 +15,11 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(child: Text('${value?.toStringAsFixed(2)}')),
+        FittedBox(
+          child: Text(
+            '${value?.toStringAsFixed(2)}',
+          ),
+        ),
         SizedBox(height: 5),
         Container(
           height: 60,
@@ -29,7 +33,7 @@ class ChartBar extends StatelessWidget {
                     color: Colors.grey,
                     width: 1.0,
                   ),
-                  color: Color.fromRGBO(0, 99, 0, 95),
+                  color: Color.fromRGBO(220, 220, 220, 1),
                   borderRadius: BorderRadius.circular(5),
                 ),
               ),
@@ -41,7 +45,7 @@ class ChartBar extends StatelessWidget {
                     borderRadius: BorderRadius.circular(5),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
